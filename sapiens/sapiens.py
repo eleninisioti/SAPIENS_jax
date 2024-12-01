3d7b9c1f791356a4fa6c66ebc750b1c757c5ec45
eleni <elennisioti@gmail.com>
2024-12-01 19:04:22 +0100

    fix

--- sapiens/sapiens.py
+++ sapiens/sapiens.py
@@ -817,7 +817,7 @@ def main(env_name , num_agents, connectivity, shared_batch_size, prob_visit, vis
     project_name =  "/sapiens_env" + env_name + "_conn_" + str(connectivity) + "_shared_batch_" + str(shared_batch_size) + "_prob_visit_" + str(prob_visit) + "_visit_dur_" + str(visit_duration) + "_n_" + str(
         num_agents) + "_trial_" + str(trial) + "_lr_" + str(learning_rate) + "_ep_10_moreexplore"
     e_start = 1.0
-    e_end = 0.5
+    e_end = 0.05
 
     wandb.login(key="575600e429b7b9e69b36d7f1584e727775d3fcfa")
 
@@ -832,7 +832,7 @@ def main(env_name , num_agents, connectivity, shared_batch_size, prob_visit, vis
 
     config = {
         "NUM_AGENTS": num_agents,
-        "BUFFER_SIZE": 5_000,
+        "BUFFER_SIZE": 25_000,
         "BUFFER_BATCH_SIZE": 256, # 64
         "SHARED_BATCH_SIZE": shared_batch_size,
         "CONNECTIVITY": connectivity,
