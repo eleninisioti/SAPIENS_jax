a1e656d7cc3bd5a42948a1eb664f582391a25f50
eleni <elennisioti@gmail.com>
2024-12-01 19:21:30 +0100

    fix

--- scripts/run_server_hpc.py
+++ scripts/run_server_hpc.py
@@ -29,10 +29,10 @@ def write_file(env, num_agents, learning_rate, connectivity, shared_batch_size, 
         file.write("#SBATCH --output=" + top_dir + "jz_logs" + output_file + "\n")
         error_file = name  + "%j.err"
         file.write("#SBATCH --error=" + top_dir + "jz_logs" + error_file+ "\n")
+        file.write("source /home/enis/.bashrc"+ "\n")
 
         file.write("module load Anaconda3 "+ "\n")
         file.write("conda activate sapiens "+ "\n")
-        file.write("source /home/enis/.bashrc"+ "\n")
 
 
         file.write("")
