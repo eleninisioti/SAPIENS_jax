a30e2db811bad23d09050180e9ba15e908d7d806
eleni <elennisioti@gmail.com>
2024-12-01 18:48:02 +0100

    fix

--- scripts/run_server.py
+++ scripts/run_server.py
@@ -32,7 +32,7 @@ def write_file(env, num_agents, learning_rate, connectivity, shared_batch_size, 
         file.write("conda activate sapiens "+ "\n")
 
         file.write("")
-        command = "python sapiens/sapiens.py --env " + env + " --n_agents " + str(num_agents) + "  --connectivity " + connectivity + "  --connectivity " + connectivity + " --trial " + str(trial) +  " --visit_duration " + str(visit_duration) + " --prob_visit " + str(prob_visit) + " --shared_batch_size " + str(shared_batch_size) + " --learning_rate " + str(learning_rate)
+        command = "python sapiens/sapiens.py --env " + env + " --n_agents " + str(num_agents) + "  --connectivity " + connectivity + "  --connectivity " + connectivity + " --trial " + str(trial) +  " --visit_duration " + str(visit_duration) + " --prob_visit " + str(prob_visit) + " --shared_batch_size " + str(shared_batch_size) + " --learning_rate " + str(learning_rate) + " --local_mode"
         file.write(command+ "\n")
 
 
