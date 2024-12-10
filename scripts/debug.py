371f39c0797903ce49d3630a165322586ac1777c
eleni <elennisioti@gmail.com>
2024-12-10 10:13:38 +0100

    fix

--- scripts/debug.py
+++ scripts/debug.py
@@ -4,13 +4,13 @@ import matplotlib.pyplot as plt
 
 project_dir = "projects/2024_12_08/sapiens_envMerging-paths-alchemy_conn_dynamic_shared_batch_1_prob_visit_0.01_visit_dur_160_n_10_trial_0_lr_0.0001_rew_8"
 #project_dir = "projects/2024_12_08/sapiens_envMerging-paths-alchemy_conn_dynamic_shared_batch_1_prob_visit_0.5_visit_dur_160_n_10_trial_0_lr_0.0001_rew_8"
-
+project_dir = "/lustre/fsn1/projects/rech/imi/utw61ti/sapiens_log/projects/2024_12_09/sapiens_envMerging-paths-alchemy_conn_dynamic_shared_batch_1_prob_visit_0.01_visit_dur_320_n_10_trial_0_lr_0.0001_rew_8"
 def check_neighbors():
 
     num_steps = 500
     num_agents = 10
     neighbors = [onp.zeros((num_steps, 2)) for _ in range(num_agents)]
-    for step in range(1,num_steps):
+    for step in range(1,num_steps, 100):
         with open(project_dir + "/neighbors/step_" + str(step) + ".pkl", "rb") as f:
             step_neighbors = pickle.load(f)
             for row in range(step_neighbors.shape[0]):
@@ -49,7 +49,7 @@ def check_metrics():
     num_steps = 2000
     num_agents = 10
     neighbors = [onp.zeros((num_steps, )) for _ in range(num_agents)]
-    for step in range(1,num_steps):
+    for step in range(1,num_steps, 100):
         with open(project_dir + "/metrics/step_" + str(step) + ".pkl", "rb") as f:
             step_neighbors = pickle.load(f)
             for row in range(step_neighbors.shape[0]):
