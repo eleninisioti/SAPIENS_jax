950768002426320cdd4b6549e953f859068abacd
eleni <elennisioti@gmail.com>
2025-01-10 01:09:53 +0200

    fix

--- scripts/run_server.py
+++ scripts/run_server.py
@@ -30,7 +30,7 @@ def write_file(env, num_agents,  connectivity, shared_batch_size, prob_visit, vi
         error_file = name  + "%j.err"
         file.write("#SBATCH --error=" + top_dir + "jz_logs" + error_file+ "\n")
         file.write("source ~/.bashrc"+ "\n")
-        file.write("module load python-3.11.5 "+ "\n")
+        file.write("module load python/3.11.5 "+ "\n")
 
         file.write("")
         command = "python sapiens/sapiens.py --env " + env + " --n_agents " + str(num_agents) + "  --connectivity " + connectivity + "  --connectivity " + connectivity + " --trial " + str(trial) +  " --visit_duration " + str(visit_duration) + " --prob_visit " + str(prob_visit) + " --shared_batch_size " + str(shared_batch_size)
